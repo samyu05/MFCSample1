209cbf446fcda61c3868db82f7038f0e4d7121bd
samyu05 <sheelamsamyuktha05@gmail.com>
2024-02-21 08:33:03 +0530

    Add project files.

---
+++ MFCSample1/Source.cpp
@@ -0,0 +1,21 @@
+#include<afxwin.h>
+//first global object will be created
+myapp obj;
+
+int __stdcall AfxWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR nCmdLine, int nCmdShow)
+{
+	//reads global object address(CWinApp derived class)
+	CWinApp* pApp = AfxGetApp();
+
+	//copies WinMain parameters to CWinApp global object
+	AfxwinInit(hInstance, hPrevInstance, nCmdLine, nCmdShow);
+	pApp->InitApplication();
+
+	//InitInstance will launch the Main Window
+	if (!pApp->InitInstance())
+		pApp->ExitInstance();
+
+	//starts message loop
+	pApp->Run();
+
+}
